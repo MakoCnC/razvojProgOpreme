6762dfd08a005707b9a10e6b00ec44ab38221260
Matej Kušej <matejkmatejk@gmail.com>
2025-10-22 19:49:50 +0200

    dodal fibonaci folder kot navadni direktori

---
+++ fibonaci/fibonaci/fibonaci.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, first = 0, second = 1, next;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Sequence: ";
+
+    for (int i = 0; i < n; ++i) {
+        if (i <= 1)
+            next = i;
+        else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        cout << next << " " haleluja; 
+    }
+
+    cout << endl;
+    return 0;
+}
